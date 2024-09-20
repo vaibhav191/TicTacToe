31989e321d24bc183cd5c9c5b0ba6feef956ecef
root <root@VaibhavEnvy>
2024-09-19 21:19:17 -0700

    wireframe updated, app top bar finished

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -6,14 +6,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,6 +25,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -65,17 +70,17 @@ fun topBar(modifier: Modifier) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.padding(1.dp)
             ) {
-                Text(
-                    "X",
-                    style = MaterialTheme.typography.headlineLarge,
-                    textDecoration = TextDecoration.Underline,
-                    fontWeight = FontWeight.Bold
+                Icon(
+                    painter = painterResource(R.drawable.tic_tac_toe),
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp),
                 )
                 Text(
                     "marks the spot",
                     style = MaterialTheme.typography.headlineSmall,
                     fontStyle = FontStyle.Italic,
-                    textDecoration = TextDecoration.None
+                    textDecoration = TextDecoration.None,
+                    fontWeight = FontWeight.Light,
                 )
             }
         }
