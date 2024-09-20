8e4d5f0d0c7a8e5b1523944866e329404aa79d06
root <root@VaibhavEnvy>
2024-09-19 21:33:58 -0700

    main page base elements

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -12,17 +12,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -54,6 +63,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun main(modifier: Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Ready for a Challenge?!")
+        // Slider
+        slider(Modifier)
+        Card(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Single Player")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Multiplayer")
+                }
+            }
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Records")
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -85,5 +120,29 @@ fun topBar(modifier: Modifier) {
             }
         }
     )
+}
+
+@Composable
+fun slider(modifier: Modifier) {
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier) {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it },
+            colors = SliderDefaults.colors(
+                thumbColor = MaterialTheme.colorScheme.secondary,
+                activeTrackColor = MaterialTheme.colorScheme.secondary,
+                inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            steps = 1,
+            valueRange = 0f..2f
+        )
+        val difficulty = when (sliderPosition.toInt()) {
+            0 -> "Easy"
+            1 -> "Medium"
+            else -> "Hard"
+        }
+        Text(text = difficulty)
+    }
 }
 
