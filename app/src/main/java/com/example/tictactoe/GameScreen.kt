dfaec30ea0e7140d1aabd652c19e63909a6cc9da
root <root@VaibhavEnvy>
2024-09-20 12:24:40 -0700

    game screen frame completed

--- app/src/main/java/com/example/tictactoe/GameScreen.kt
+++ app/src/main/java/com/example/tictactoe/GameScreen.kt
@@ -4,22 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tictactoe.ui.theme.TicTacToeTheme
 
 class GameScreen : ComponentActivity() {
@@ -40,30 +51,178 @@ class GameScreen : ComponentActivity() {
 
 @Composable
 fun Board(modifier: Modifier) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row {
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(Color.White),
-                modifier = Modifier.border(1.dp, Color.Black, RoundedCornerShape(5.dp))
-            ) {
+    Column {
+        Column(
 
+            modifier = Modifier
+                .padding(top = 100.dp, start = 50.dp, end = 50.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        )
+        {
+            Text(
+                text = "Hard",
+                fontStyle = FontStyle.Italic,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(500.dp)
+                .clip(RoundedCornerShape(40.dp))
+                .padding(start = 20.dp, end = 20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            val buttonColor = MaterialTheme.colorScheme.primaryContainer
+            val buttonElevation = 10.dp
+            Row(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color.Black)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(end = 5.dp, bottom = 5.dp),
+                    shape = RectangleShape,
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.cross),
+                        contentDescription = null
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(end = 5.dp, bottom = 5.dp),
+
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+                    Image(painter = painterResource(id = R.drawable.o), contentDescription = null)
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(bottom = 5.dp),
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+
+                }
             }
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(Color.White)
+
+            Row(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color.Black)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
 
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(end = 5.dp, bottom = 5.dp),
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(end = 5.dp, bottom = 5.dp),
+
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(bottom = 5.dp),
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+
+                }
             }
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(Color.White)
+
+            Row(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color.Black)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
 
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(end = 5.dp),
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .padding(end = 5.dp),
+
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(buttonColor),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f),
+                    elevation = ButtonDefaults.buttonElevation(defaultElevation = buttonElevation),
+                    shape = RectangleShape
+                ) {
+
+                }
             }
         }
     }
--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,18 +2,13 @@ package com.example.tictactoe
 
 import android.content.Context
 import android.content.Intent
-import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -27,7 +22,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -36,14 +30,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tictactoe.ui.theme.TicTacToeTheme
@@ -73,10 +64,11 @@ fun main(modifier: Modifier, context: Context = LocalContext.current) {
         verticalArrangement = Arrangement.Center
     ) {
         // ready for a challenge
-        Column(modifier = Modifier
-            .weight(1f)
-            .fillMaxSize()
-            .padding(top = 100.dp),
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .padding(top = 100.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -88,6 +80,7 @@ fun main(modifier: Modifier, context: Context = LocalContext.current) {
                 modifier = Modifier
             )
         }
+
         //card for single player
         Card(
             modifier = Modifier
@@ -170,7 +163,7 @@ fun slider(modifier: Modifier) {
                 thumbColor = MaterialTheme.colorScheme.secondary,
                 activeTrackColor = MaterialTheme.colorScheme.secondary,
                 inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
-                ),
+            ),
 
             steps = 1,
             valueRange = 0f..2f
