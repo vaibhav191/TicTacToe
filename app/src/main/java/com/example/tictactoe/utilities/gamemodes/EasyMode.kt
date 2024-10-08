666418f5f231b16b12742f6eb36b64058288e237
root <root@VaibhavEnvy>
2024-10-08 13:50:08 -0700

    fix: updating easy game mode with moveAI function and dependency

--- app/src/main/java/com/example/tictactoe/utilities/gamemodes/EasyMode.kt
+++ app/src/main/java/com/example/tictactoe/utilities/gamemodes/EasyMode.kt
@@ -1,20 +1,57 @@
 package com.example.tictactoe.utilities.gamemodes
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.example.tictactoe.utilities.abstracts.GameMode
 import com.example.tictactoe.utilities.enums.GameResultEnum
 import com.example.tictactoe.utilities.enums.MovesEnum
 import com.example.tictactoe.utilities.enums.PlayersEnum
+import com.example.tictactoe.utilities.enums.StatesEnum
 import com.example.tictactoe.utilities.gameobjs.Game
 import com.example.tictactoe.utilities.gameobjs.PlayerInGame
+import com.example.tictactoe.utilities.randomselector.SelectRandom
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class EasyMode: GameMode() {
+class EasyMode : GameMode() {
     override val playerX = PlayerInGame("Player X", PlayersEnum.X)
     override val playerO = PlayerInGame("Player O", PlayersEnum.O)
-    override val game = Game(playerX, playerO, board)
+    override var game  = Game(playerX, playerO, board)
     override var turn_X = true
 
+
     override fun move(move: MovesEnum): GameResultEnum {
+        Log.d("EasyMode", "move: $move")
         /* To be implemented */
-       return GameResultEnum.NotOver
+        if (this.turn_X) {
+            Log.d("EasyMode", "turn_X: $turn_X")
+            game.move(true, false, move)
+            Log.d("EasyMode", "user move rendered: $move")
+            this.turn_X = false
+            Log.d("EasyMode", "flipped turn_X: $turn_X")
+        } else {
+            Log.d("EasyMode", "turn_X: $turn_X")
+            // randomly select from available moves
+
+            game.move(false, true, move)
+            Log.d("EasyMode", "computer move rendered: $move")
+            this.turn_X = true
+            Log.d("EasyMode", "flipped turn_X: $turn_X")
+        }
+        val gameState = game.checkWinner()
+        Log.d("EasyMode", "game state: $gameState")
+        return gameState
+    }
+
+    fun moveAI() {
+        Log.d("EasyMode", "moveAI")
+        val availableMoves =
+            game.board.availableMoves.moves.filter { it.state == StatesEnum.AVAILABLE }
+        val randomMove = SelectRandom.select(availableMoves.map { it.move })
+        move(randomMove)
     }
 }
--- app/src/main/java/com/example/tictactoe/utilities/randomselector/SelectRandom.kt
+++ app/src/main/java/com/example/tictactoe/utilities/randomselector/SelectRandom.kt
@@ -1,12 +1,16 @@
 package com.example.tictactoe.utilities.randomselector
 
+import android.util.Log
 import com.example.tictactoe.utilities.enums.MovesEnum
 
 class SelectRandom {
     companion object {
         fun select(availableMoves: List<MovesEnum>): MovesEnum {
+            Log.d("SelectRandom", "available moves: $availableMoves")
             if (availableMoves.isNotEmpty()) {
-                return availableMoves.random()
+                val random = availableMoves.random()
+                Log.d("SelectRandom", "random move: $random")
+                return random
             }
             throw Exception("No available moves")
         }
