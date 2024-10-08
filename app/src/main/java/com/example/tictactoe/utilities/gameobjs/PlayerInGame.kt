3e968a5ef35dcca797118f151d260685597c0298
root <root@VaibhavEnvy>
2024-10-08 11:39:25 -0700

    chore: new package game objs to hold classes necessary for game mode creation

---
+++ app/src/main/java/com/example/tictactoe/utilities/gameobjs/PlayerInGame.kt
@@ -0,0 +1,23 @@
+package com.example.tictactoe.utilities.gameobjs
+
+import com.example.tictactoe.utilities.data.MoveListData
+import com.example.tictactoe.utilities.data.MoveStateData
+import com.example.tictactoe.utilities.enums.MovesEnum
+import com.example.tictactoe.utilities.enums.PlayersEnum
+import com.example.tictactoe.utilities.enums.StatesEnum
+
+class PlayerInGame(name: String, type: PlayersEnum) {
+    var moveList: MoveListData = MoveListData(mutableListOf(
+        MoveStateData(MovesEnum.TOP_LEFT, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.TOP_CENTER, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.TOP_RIGHT, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.MIDDLE_LEFT, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.MIDDLE_CENTER, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.MIDDLE_RIGHT, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.BOTTOM_LEFT, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.BOTTOM_CENTER, StatesEnum.AVAILABLE),
+        MoveStateData(MovesEnum.BOTTOM_RIGHT, StatesEnum.AVAILABLE)
+    ))
+    val playerType: PlayersEnum = type
+    val playerName: String = name
+}
