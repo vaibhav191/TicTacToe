8adf0a4ca7e5ca02308bc8666bc9ee338d080099
root <root@VaibhavEnvy>
2024-10-08 12:22:45 -0700

    feat: now has a method get using value, along with values to go with each enum

--- app/src/main/java/com/example/tictactoe/utilities/enums/ConnectionTypeEnum.kt
+++ app/src/main/java/com/example/tictactoe/utilities/enums/ConnectionTypeEnum.kt
@@ -1,6 +1,11 @@
 package com.example.tictactoe.utilities.enums
 
-enum class ConnectionTypeEnum {
-    Local,
-    Bluetooth
+enum class ConnectionTypeEnum(val value: Int) {
+    Local(0),
+    Bluetooth(1);
+    companion object {
+        fun getConnectionType(value: Int): ConnectionTypeEnum? {
+            return entries.find { it.value == value }
+        }
+    }
 }
--- app/src/main/java/com/example/tictactoe/utilities/enums/LocalDifficultyEnum.kt
+++ app/src/main/java/com/example/tictactoe/utilities/enums/LocalDifficultyEnum.kt
@@ -1,8 +1,17 @@
 package com.example.tictactoe.utilities.enums
 
-enum class LocalDifficultyEnum {
-    Easy,
-    Medium,
-    Hard,
-    PlayervsPlayer
+import android.util.Log
+
+enum class LocalDifficultyEnum(val value: Int) {
+    Easy(0),
+    Medium(1),
+    Hard(2),
+    PlayervsPlayer(3);
+
+    companion object {
+        fun getDifficulty(value: Int): LocalDifficultyEnum? {
+            Log.d("LocalDifficultyEnum", "getDifficulty: $value")
+            return entries.find { it.value == value }
+        }
+    }
 }
