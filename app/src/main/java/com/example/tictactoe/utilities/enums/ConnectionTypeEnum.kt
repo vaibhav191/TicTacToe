6e9037ad142820caa9a53e9b8e88401ff963351e
root <root@VaibhavEnvy>
2024-10-08 12:59:40 -0700

    fix: closing bracket

--- app/src/main/java/com/example/tictactoe/utilities/enums/ConnectionTypeEnum.kt
+++ app/src/main/java/com/example/tictactoe/utilities/enums/ConnectionTypeEnum.kt
@@ -3,8 +3,10 @@ package com.example.tictactoe.utilities.enums
 enum class ConnectionTypeEnum(val value: Int) {
     Local(0),
     Bluetooth(1);
+
     companion object {
         fun getConnectionType(value: Int): ConnectionTypeEnum? {
             return entries.find { it.value == value }
         }
     }
+}
