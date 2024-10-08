d7211fbba6946fa7205882542c560a558909514c
root <root@VaibhavEnvy>
2024-10-08 13:12:33 -0700

    chore: placeholder difficulties

---
+++ app/src/main/java/com/example/tictactoe/utilities/gamemodes/BluetoothPlayervsPlayer.kt
@@ -0,0 +1,20 @@
+package com.example.tictactoe.utilities.gamemodes
+
+import com.example.tictactoe.utilities.abstracts.GameMode
+import com.example.tictactoe.utilities.enums.GameResultEnum
+import com.example.tictactoe.utilities.enums.MovesEnum
+import com.example.tictactoe.utilities.enums.PlayersEnum
+import com.example.tictactoe.utilities.gameobjs.Game
+import com.example.tictactoe.utilities.gameobjs.PlayerInGame
+
+class BluetoothPlayervsPlayer: GameMode() {
+    override val playerX = PlayerInGame("Player X", PlayersEnum.X)
+    override val playerO = PlayerInGame("Player O", PlayersEnum.O)
+    override val game = Game(playerX, playerO, board)
+    override var turn_X = true
+
+    override fun move(move: MovesEnum): GameResultEnum {
+        /* To be implemented */
+        return GameResultEnum.NotOver
+    }
+}
--- app/src/main/java/com/example/tictactoe/utilities/interfaces/GameMode.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tictactoe.utilities.interfaces
-
-abstract class GameMode {
-}
