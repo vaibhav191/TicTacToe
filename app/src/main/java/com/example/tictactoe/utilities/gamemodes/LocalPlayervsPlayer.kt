f8afa9924104d163c3f620b4f69276bfab782dca
root <root@VaibhavEnvy>
2024-10-10 18:10:30 -0700

    fx: local player vs player is now passing necessary parameters to super class

--- app/src/main/java/com/example/tictactoe/utilities/gamemodes/LocalPlayervsPlayer.kt
+++ app/src/main/java/com/example/tictactoe/utilities/gamemodes/LocalPlayervsPlayer.kt
@@ -1,16 +1,22 @@
 package com.example.tictactoe.utilities.gamemodes
 
+import android.util.Log
 import com.example.tictactoe.utilities.gameobjs.Game
 import com.example.tictactoe.utilities.gameobjs.PlayerInGame
 import com.example.tictactoe.utilities.abstracts.GameMode
 import com.example.tictactoe.utilities.enums.GameResultEnum
 import com.example.tictactoe.utilities.enums.MovesEnum
 import com.example.tictactoe.utilities.enums.PlayersEnum
+import com.example.tictactoe.utilities.gameobjs.Board
 
-class LocalPlayervsPlayer: GameMode() {
-    override val playerX = PlayerInGame("Player X", PlayersEnum.X)
-    override val playerO = PlayerInGame("Player O", PlayersEnum.O)
-    override val game = Game(playerX, playerO, board)
+class LocalPlayervsPlayer: GameMode(
+    playerX = PlayerInGame("Player X", PlayersEnum.X),
+    playerO = PlayerInGame("Player O", PlayersEnum.O),
+    board = Board()
+) {
+    init {
+        Log.d("LocalPlayervsPlayer", "LocalPlayervsPlayer called")
+    }
     override var turn_X = true
 
     override fun move(move: MovesEnum): GameResultEnum {
